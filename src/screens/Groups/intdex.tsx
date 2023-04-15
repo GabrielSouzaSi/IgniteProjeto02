1c4711e54534563be3cee3554e8e00bd676c0d82
gabriel <gos.gabri@gmail.com>
2023-04-15 01:02:44 -0400

    Componente List Empty

--- src/screens/Groups/intdex.tsx
+++ src/screens/Groups/intdex.tsx
@@ -4,12 +4,12 @@ import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 import { Container } from "./styles";
-import { G } from "react-native-svg";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -19,15 +19,11 @@ export function Groups() {
 
       <FlatList
         data={groups}
-        keyExtractor={item => item}
-        renderItem={({item}) => (
-          <GroupCard
-            title={item}
-          />
-        )
-      }
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1}}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
       />
-
     </Container>
   );
 }
