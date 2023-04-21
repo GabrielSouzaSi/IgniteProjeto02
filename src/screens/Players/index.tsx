100d36728d4fd63449892f9026c9bf1367a0be4f
gabriel <gos.gabri@gmail.com>
2023-04-20 22:38:58 -0400

    Componente Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
@@ -21,6 +22,8 @@ export function Players() {
         <ButtonIcon icon="add" />
       </Form>
       
+      <Filter title="Time A" isActive />
+
     </Container>
   );
 }
