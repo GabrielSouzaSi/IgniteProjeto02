2b8180cc366a1b508620369098b8706073b1e5a0
gabriel <gos.gabri@gmail.com>
2023-05-10 11:23:48 -0400

    Identificando o foco na tela

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { FlatList } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -9,6 +9,7 @@ import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
 import { Container } from "./styles";
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -16,8 +17,24 @@ export function Groups() {
   const navigation = useNavigation();
 
   function handleNewGroup() {
-    navigation.navigate('new');
+    navigation.navigate("new");
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log("useFocusEffect execuou");
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <Container>
@@ -29,15 +46,13 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
-        contentContainerStyle={groups.length === 0 && { flex: 1}}
-        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
         showsHorizontalScrollIndicator={false}
       />
-      <Button 
-        title="Criar nova turmas"
-        onPress={handleNewGroup}
-      />
+      <Button title="Criar nova turmas" onPress={handleNewGroup} />
     </Container>
   );
 }
- 
